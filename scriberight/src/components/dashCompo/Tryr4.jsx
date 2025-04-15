22ff7d051fedd3d1c55767d2c73c5cd0cea71d86
jayksgit <jayk290452@gmail.com>
2025-04-15 18:55:44 +0530

    removing unwanted try4 files

--- scriberight/src/components/dashCompo/Tryr4.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-export default function Tryr3() {
-  return (
-    <div>
-      Tryr3 <Link to='/try4'></Link>
-    </div>
-  )
-}
