88f78ece955f69146a4e1b30dc09c170efa63de5
jayksgit <jayk290452@gmail.com>
2025-04-15 18:54:58 +0530

    removing unwanted try files

--- scriberight/src/components/dashCompo/Tryr2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-export default function Tryr2() {
-  return (
-    <div>
-      Tryr2<Link to='/try1'></Link>
-    </div>
-  )
-}
--- scriberight/src/components/dashCompo/Tryr3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Tryr3() {
-  return (
-    <div>
-      Tryr3
-    </div>
-  )
-}
